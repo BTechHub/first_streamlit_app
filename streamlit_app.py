0dcab8f6083615ec3780f187d2134fec75e8c985
BTechHub <113085638+BTechHub@users.noreply.github.com>
2022-09-09 20:05:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 
 my_fruit_list=my_fruit_list.set_index("Fruit")
 
-st.multiselect("pick some fruits",list(my_fruit_list.index))
+st.multiselect("pick some fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 st.dataframe(my_fruit_list)
